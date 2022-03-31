8e1e981b0bb686a019af30b29875503819f43298
phuongnamswdhg <92508760+phuongnamswdhg@users.noreply.github.com>
2022-03-31 18:00:55 +0700

    https://github.com/phuongnamswdhg/baithuchanh.git

---
+++ phonebook/Phone.java
@@ -0,0 +1,9 @@
+package phonebook;
+
+public class Phone {
+    abstract void insertPhone (String name, String phone);
+    abstract void removePhone (String name);
+    abstract void updatePhone (String name, String newPhone);
+    abstract void searchPhone (String name);
+    abstract void sort() ;
+}
